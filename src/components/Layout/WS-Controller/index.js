c97ae0ca21ad2fd754391928ebeb15e50875ba15
Chriswf <chriswfoster@gmail.com>
2021-10-13 22:50:41 -0500

    socket update

--- src/components/Layout/WS-Controller/index.js
+++ src/components/Layout/WS-Controller/index.js
@@ -1,45 +1,16 @@
-import React, {useState, useCallback, useRef, useMemo} from 'react';
+import React, {useState, useCallback, useRef, useMemo, useEffect} from 'react';
 // import ws from '../../../core/ws-socket';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
+import socket from '../../../core/socket-io';
 
  
 const WSController = () => {
-    const [socketUrl, setSocketUrl] = useState('ws://localhost:8080');
-    const messageHistory = useRef([]);
 
-    const {
-        sendMessage,
-        sendJsonMessage,
-        lastMessage,
-        lastJsonMessage,
-        readyState,
-        getWebSocket
-      } = useWebSocket(socketUrl, {
-        onOpen: () => console.log('opened'),
-        //Will attempt to reconnect on all close events, such as server shutting down
-        shouldReconnect: (closeEvent) => true,
-      });
-
-      messageHistory.current = useMemo(() =>
-      messageHistory.current.concat(lastMessage),[lastMessage]);
-  
-    const handleClickChangeSocketUrl = useCallback(() =>
-      setSocketUrl('wss://demos.kaazing.com/echo'), []);
-  
-    const handleClickSendMessage = useCallback(() =>
-      sendMessage('Hello'), []);
-    // ws.on('open', function open() {
-    //     ws.send('something');
-    //   });
-    const connectionStatus = {
-        [ReadyState.CONNECTING]: 'Connecting',
-        [ReadyState.OPEN]: 'Open',
-        [ReadyState.CLOSING]: 'Closing',
-        [ReadyState.CLOSED]: 'Closed',
-        [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-      }[readyState];
-
-
+    useEffect(() => {
+        console.log('eyyyy')
+        socket.on("test", (data) => {
+            console.log("test happened: ", data)
+        });
+    }, []);
 
     return (
         <div>This is the WSController component.</div>
