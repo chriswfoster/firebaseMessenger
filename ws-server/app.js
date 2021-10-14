b82ef7f510bae9077ddb83454a6e00fb1bcfb61f
Chriswf <chriswfoster@gmail.com>
2021-10-13 23:21:23 -0500

    setting up other server

--- ws-server/app.js
+++ ws-server/app.js
@@ -1,46 +1,31 @@
 
 const express = require('express');
-const cors = require('cors');
-const WebSocket = require('ws');
-const {WebSocketServer} = require('ws');
-
-
-const wss = new WebSocketServer({
-    port: 8080,
-    perMessageDeflate: {
-      zlibDeflateOptions: {
-        // See zlib defaults.
-        chunkSize: 1024,
-        memLevel: 7,
-        level: 3
-      },
-      zlibInflateOptions: {
-        chunkSize: 10 * 1024
-      },
-      // Other options settable:
-      clientNoContextTakeover: true, // Defaults to negotiated value.
-      serverNoContextTakeover: true, // Defaults to negotiated value.
-      serverMaxWindowBits: 10, // Defaults to negotiated value.
-      // Below options specified as default values.
-      concurrencyLimit: 10, // Limits zlib concurrency for perf.
-      threshold: 1024 // Size (in bytes) below which messages
-      // should not be compressed if context takeover is disabled.
-    }
-  });
-
-  wss.on('connection', function connection(ws) {
-      console.log("SOMEONE CONNECTED")
-    ws.on('message', function incoming(message) {
-      console.log('received: %s', message);
-    });
-  
-    ws.send('something');
-  });
-
 const app = express();
+const cors = require('cors');
 app.use(cors());
+const ws = require('./controllers/ws-server');
+const socketIo = require('./controllers/socket-io');
+const http = require('http');
+const server = http.createServer(app);
+
+// ws();
+// socketIo(server);
+
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+
+
 const port = 1738;
 
-app.listen(port, () => {
-    console.log("Tuned in to smooth jazz: ", port)
+// app.listen(port, () => {
+//     console.log("Tuned in to smooth jazz: ", port)
+// });
+server.listen(port, () => {
+  console.log('listening on *:', port);
 });
